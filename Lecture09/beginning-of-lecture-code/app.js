5928635945365490624bcefa8e526a249ad408fe
hcone5006 <heidicone@gmail.com>
2016-10-02 18:29:49 +1300

    adding assignment 1 and lessons 10 and 11

--- Lecture09/beginning-of-lecture-code/app.js
+++ Lecture09/beginning-of-lecture-code/app.js
@@ -4,8 +4,19 @@
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
-function DIController ($scope) {
+function DIController ($scope, $filter) {
   $scope.name = "Yaakov";
+
+  $scope.upper = function (){
+    var upCase = $filter('uppecase');
+    $scope.name = upCase($scope.name);
+  };
 }
+
+function AnnonateMe(name, job, blah) {
+  return "Blah!";
+}
+
+console.log(AnnonateMe);
 
 })();
---
+++ Lecture11/beginning-of-lecture-code/part1/app.js
@@ -0,0 +1,17 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.name = "Yaakov";
+
+  $scope.sayMessage = function (){
+    return "Yaakov likes to eat healthy snacks at night."
+  }
+
+}
+
+})();
